610d2f8f8a750d87ed60390d8e187782cb9f86fd
Juan Pablo <francopablo155@gmail.com>
2025-03-11 11:07:57 -0500

    First commit, added method add tag

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,15 @@
-# TODO: Add code here
+class Todo:
+
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str =  title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags :list[str] =[]
+
+    def mark_completed(self):
+        self.completed = True
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
