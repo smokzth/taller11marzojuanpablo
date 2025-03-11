d8126309e8253a6589d9f0779d626401c9db2068
Juan Pablo <francopablo155@gmail.com>
2025-03-11 11:48:31 -0500

    commit final clase 11 marzo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -12,8 +12,13 @@ class Todo:
     def add_tag(self, tag: str):
         if tag not in self.tags:
             self.tags.append(tag)
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.code_id} {self.title}"
 class TodoBook:
-    
+    def __init__(self):
+        self.todos : dict[int: Todo] = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        new_id = len(self.todos) +1
+
 
